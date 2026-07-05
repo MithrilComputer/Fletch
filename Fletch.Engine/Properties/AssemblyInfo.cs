e35e302eee27eb67a39f8227e3dff06ae9169e5a
Drew Terwilliger <120346155+MithrilComputer@users.noreply.github.com>
2026-07-05 22:33:22 +0200

    Added Physics System

--- Fletch.Engine/Properties/AssemblyInfo.cs
+++ Fletch.Engine/Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 
 [assembly: InternalsVisibleTo("Fletch.Rendering")]
 [assembly: InternalsVisibleTo("Fletch.Audio")]
+[assembly: InternalsVisibleTo("Fletch.Physics")]
 [assembly: InternalsVisibleTo("Fletch.Input")]
 [assembly: InternalsVisibleTo("Fletch.Runtime")]
 [assembly: InternalsVisibleTo("Fletch.Platform.MonoGame")]
---
+++ Fletch.Physics/Systems/PhysicsSystem.cs
@@ -0,0 +1,34 @@
+﻿using Fletch.Engine.Components;
+using Fletch.Engine.Model;
+using Fletch.Engine.Scenes;
+using Fletch.Engine.Systems;
+using Fletch.Physics.Components;
+using Fletch.Physics.Components.CollisionShapes;
+
+namespace Fletch.Physics.Systems
+{
+    internal class PhysicsSystem : SceneSubsystem
+    {
+
+
+        public override void AttachToScene(Scene scene)
+        {
+            base.AttachToScene(scene);
+
+            scene.SystemManager.RegisterSystem(this, SystemExecutionOrder.Simulation, 0);
+
+            scene.AddSystemComponentRegistration(typeof(RigidBody), (b, c) => OnRigidBodyChange(b, c));
+            scene.AddSystemComponentRegistration(typeof(BoxCollider), (b, c) => OnColliderChange(b, c));
+        }
+
+        private void OnRigidBodyChange(GameObjectComponent component, ComponentChangeType changeType)
+        {
+
+        }
+
+        private void OnColliderChange(GameObjectComponent component, ComponentChangeType changeType)
+        {
+
+        }
+    }
+}
